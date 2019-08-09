c28ddc84e15413c953c369852d84209b15585198
jemarle95 <jemarle@hotmail.co.uk>
2019-08-09 17:40:22 +0100

    Created A new ViewController

---
+++ GitTutorial/ExampleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExampleViewController.swift
+//  GitTutorial
+//
+//  Created by Jermal Ebanks on 09/08/2019.
+//  Copyright © 2019 Micaela Ribeiro | WECORP. All rights reserved.
+//
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
