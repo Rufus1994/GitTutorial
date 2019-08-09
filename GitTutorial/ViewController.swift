11e829046ccdbb8c90d71797199776081f522166
Micaela Ribeiro <micaela.ribeiro@wecorp.com>
2019-08-09 16:22:13 +0100

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Micaela Ribeiro | WECORP on 09/08/2019.
+//  Copyright © 2019 Micaela Ribeiro | WECORP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
